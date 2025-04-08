0e145514daa34b705567ea52aeb1cdfaeb83014e
Fathia Izdzati <izdzati@gmail.com>
2025-04-08 11:55:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from google.oauth2.service_account import Credentials
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 
-# Ambil secret dari Streamlit
-service_account_info = json.loads(st.secrets["gcp_service_account"])
+service_account_info = st.secrets["gcp_service_account"]
+
 
 # Autentikasi
 creds = Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
