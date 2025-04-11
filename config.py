72abe761475218b8579715e743504181d568d644
Fathia Izdzati <izdzati@gmail.com>
2025-04-11 15:24:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,20 @@
+import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Load Credentials dari JSON
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-creds = Credentials.from_service_account_file("/content/Konversi/konversi-jateng-984cb852b8dd.json", scopes=SCOPES)
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets", 
+          "https://www.googleapis.com/auth/drive"]
 
-# Autentikasi ke Google Sheets
+# Get service account info from Streamlit secrets
+service_account_info = dict(st.secrets["gcp_service_account"])
+
+# Fix newlines in private key if needed
+if '\\n' in service_account_info['private_key']:
+    service_account_info['private_key'] = service_account_info['private_key'].replace('\\n', '\n')
+
+# Authenticate
+creds = Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
 client = gspread.authorize(creds)
 
-# Akses Google Sheet
 SHEET_NAME = "KONVERSI JATENG"
 sheet = client.open(SHEET_NAME).sheet1
