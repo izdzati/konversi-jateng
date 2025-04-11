a930b4994afd3b56689c8d42fabbff7da35f107b
Fathia Izdzati <izdzati@gmail.com>
2025-04-11 15:40:24 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from google.oauth2.service_account import Credentials
 
 # Load Credentials dari JSON
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-creds = Credentials.from_service_account_file("konversi-jateng/konversi-jateng-984cb852b8dd.json", scopes=SCOPES)
+creds = Credentials.from_service_account_file("konversi-jateng-984cb852b8dd.json", scopes=SCOPES)
 
 # Autentikasi ke Google Sheets
 client = gspread.authorize(creds)
